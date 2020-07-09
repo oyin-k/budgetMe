0481a5a28c28de208feae63c87592c91516967e3
oyin-k <oyink7@gmail.com>
2020-07-10 00:45:17 +0100

    delete confirmation modal component added

--- budget-me/src/components/AccountItem/AccountItem.js
+++ budget-me/src/components/AccountItem/AccountItem.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import Modal from '../Modal/Modal';
 
 import { formatNumber } from '../../helpers/formatNumber';
 
@@ -10,6 +12,10 @@ const AccountItem = ({
   children,
   deleteItem,
 }) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const toggleModal = () => setShowModal(!showModal);
+
   const handleDelete = () => {
     deleteItem(id, amount);
   };
@@ -21,10 +27,32 @@ const AccountItem = ({
         <div className="item__value">{formatNumber(amount, type)}</div>
         {children}
         <div className="item__delete">
-          <button className="item__delete--btn" onClick={handleDelete}>
+          <button className="item__delete--btn" onClick={toggleModal}>
             <i className="ion-ios-close-outline"></i>
           </button>
         </div>
+        {showModal ? (
+          <Modal>
+            <h3>Are you sure you want to delete this item?</h3>
+            <div className="item__details">
+              <p className="item__details--description">{description}</p>
+              <p className="item__details--amount">
+                {formatNumber(amount, type)}
+              </p>
+            </div>
+            <div className="modal__buttons">
+              <button className="modal__buttons--cancel" onClick={toggleModal}>
+                Cancel
+              </button>
+              <button
+                className="modal__buttons--confirm"
+                onClick={handleDelete}
+              >
+                Delete
+              </button>
+            </div>
+          </Modal>
+        ) : null}
       </div>
     </div>
   );
